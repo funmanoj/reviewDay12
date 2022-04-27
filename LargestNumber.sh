f6d092c7ab57a11c9c6fd7d702dd6446706933e8
Manoj Funde <funde.manoj@gmail.com>
2022-04-27 08:39:01 +0530

    [Manoj] first problem

---
+++ LargestNumber.sh
@@ -0,0 +1,18 @@
+#! /bin/bash -x 
+
+echo "Enter the first number"
+read num1
+echo "Enter the second number"
+read num2
+echo "Enter the third number"
+read num3
+
+if [ $num1 -gt $num 2 && $num1 -lt $num3 ]
+then
+   echo "$num1 is second largest number"
+elif [ if $num2 -gt num1 && $num2 -lt $num3 ]
+   echo "$num2 is second largest number"
+else
+   echo "$num3 is second largest number"
+fi
+
